8eae30e9e0d24c30f932d7c71b127ea7df37d999
Francisco Grisanti <franciscogrisanti@FranciscosMBP2.attlocal.net>
2020-09-09 16:32:18 -0500

    Update 09/09

--- job.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "[INFO] Starting general AE"
-
-python train.py --model autoencoder --data other --data_path data/gold_standard/synthetic_batch_1_raw.h5ad --output AE_synthetic_batch_1_raw_v1
-
-python train.py --model autoencoder --data other --data_path data/gold_standard/synthetic_batch_2_raw.h5ad --output AE_synthetic_batch_2_raw_v1
-
-echo "[INFO] Finished job script"
--- train.py
+++ train.py
@@ -17,35 +17,40 @@ Date: 12/2020
 # USAGE
 # python train.py --model STANN --data seqfish --output AE_seqfish_1
 
-
-from numpy.random import seed
-seed(123)
-
-#from tensorflow import set_random_seed
-#set_random_seed(234)
-
-# import the necessary packages
-import matplotlib.pyplot as plt
+#import the necessary packages
 import logging
 import argparse
-import os
+import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+import random
+import os
 import sys
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.optimizers import SGD
+import pandas as pd
+import warnings
+import sklearn
+import seaborn as sb
+import venn
+import sklearn
+from sklearn.utils import class_weight
+from sklearn.metrics import roc_auc_score
+import pickle
+import tqdm
+import scanpy as sc
+import anndata
 import tensorflow as tf
+import tensorflow.keras as keras
 import scanpy as sc
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-
 # local functions
 from STANN.models import STANN
-from STANN.utils import cross_validate, plot_confusion_matrix
+import STANN.utils as utils
 
+logging.getLogger("tensorflow").setLevel(logging.CRITICAL)
 
-#logging.getLogger("tensorflow").setLevel(logging.CRITICAL)
+#Reproducibility
+seed = 10
+np.random.seed(seed)
+tf.random.set_seed(seed)
 
 ################construct the argument parser and parse the arguments###################
 ap = argparse.ArgumentParser()
@@ -95,6 +100,70 @@ ap.add_argument(
 
 args = vars(ap.parse_args())
 
+
+################LOAD DATA###################
+
+# check to see which data
+print("[INFO] loading training data...")
+adata_train = sc.read_h5ad("../data/scrna.h5ad")
+
+print("[INFO] loading predict data...")
+adata_predict = sc.read_h5ad("../data/seqfish.h5ad")
+
+
+
+model = STANN(act_fun='tanh',
+              first_dense=160,
+              second_dense=145.0,
+              learning_rate=0.01,input_dim=adata_train.X.shape[1],
+              output_dim=len(adata_train.obs.celltype.unique()))
+
+model.summary()
+
+
+X_train, Y_train, X_predict = utils.organize_data(adata_train=adata_train,
+                                            adata_predict=adata_predict)
+
+
+X_train_scaled , scaler_train = utils.min_max(X=X_train)
+
+X_predict_scaled , scaler_predict = utils.min_max(X=X_predict)
+
+Y_train_dummy,Y_train_ohe,encoder = utils.label_encoder(Y_train=Y_train)
+
+x_train, x_test, y_train, y_test = utils.train_test_split(X_train_scaled,
+                                                    Y_train_ohe,
+                                                    test_size=0.10, 
+                                                    random_state=40)
+
+class_weights = utils.get_class_weights(Y_train_ohe=y_train)
+
+es = tf.keras.callbacks.EarlyStopping(monitor='accuracy', 
+                                      mode='min', 
+                                      verbose=1,
+                                      patience=30)
+
+history = model.fit(x_train, 
+                    y_train, 
+                    validation_data=(x_test, y_test),
+                    epochs=30,
+                    class_weight=class_weight,
+                    callbacks=[es],verbose=0)
+
+ utils.print_metrics(model=model,
+                  x_train=x_train,
+                  y_train=y_train,
+                  x_test=x_test,
+                  y_test=y_test)
+
+predictions = utils.make_predictions(model=model,
+                     X_predict=X_predict_scaled,
+                     encoder=encoder,
+                     adata_predict=adata_predict,
+                     probabilities=False,
+                     save=True
+                    )
+                    
 ################create np array vectors###################
 def create_data(adata,cell_type_id=None,model=None,pc=False):  
     """
